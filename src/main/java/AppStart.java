ee1bb0acb068678049c65d34845114838a46f027
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-03-01 15:03:51 +0100

    singleton task
    Signed-off-by: Przemyslaw Bykowski <p.bykowski@sdacademy.pl>

--- src/main/java/AppStart.java
+++ src/main/java/AppStart.java
@@ -1,18 +1,16 @@
 public class AppStart {
 
     public static void main(String[] args) {
-        MobilePhone mobilePhone1 =
-                new MobilePhone("16 GB", "4 GB", "3200MHz", "5 inches");
-        MobilePhone mobilePhone2 =
-                new MobilePhone("8 GB", "2 GB", "2400MHz", "5.5 inches", false);
-        MobilePhone mobilePhone3 =
-                new MobilePhone("4 GB", "1 GB", "1800MHz", "4.8 inches", true, false);
-        MobilePhone mobilePhone4 =
-                new MobilePhone("32 GB", "4 GB", "2x3200MHz", "6.28 inches", true, false, false);
-
-        System.out.println(mobilePhone1);
-        System.out.println(mobilePhone2);
-        System.out.println(mobilePhone3);
-        System.out.println(mobilePhone4);
+        StringBuffer info = new StringBuffer();
+        info.append(System.lineSeparator());
+        info.append("My phone number is: ");
+        info.append(PhoneNumberInfo.number);
+        info.append(System.lineSeparator());
+        info.append("My operator is: ");
+        info.append(PhoneNumberInfo.operator);
+        info.append(System.lineSeparator());
+        info.append("My tariff is: ");
+        info.append(PhoneNumberInfo.tariff);
+        System.out.println(info);
     }
 }
---
+++ src/main/java/PhoneNumberInfo.java
@@ -0,0 +1,6 @@
+public class PhoneNumberInfo {
+
+    public static final String number = "+12 241294019";
+    public static final String operator = "MyMobile";
+    public static final String tariff = "economic";
+}
